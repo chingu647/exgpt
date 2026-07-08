b4e03ba62db0bb07ad10de2826d9715ad0755fbc
chingu647 <chingu647@gmail.com>
2026-07-08 20:55:18 +0900

    all

--- Home.py
+++ Home.py
@@ -78,7 +78,7 @@ def generate_content_with_retry(client, model, contents):
 # 메인 화면 구성 및 챗 초기화
 # ==========================================
 st.subheader("💬 휴게소 업무기준 Chatbot")
-st.caption("🚀 ex <휴게시설 업무기준> 및 <자체투자사업 매뉴얼> 안내")
+st.caption("🚀 ex <**휴게시설 업무기준**> 및 <**자체투자사업** 매뉴얼> 안내")
 
 # [임시 관리 메뉴] 저장소 비우기 사이드바
 with st.sidebar:
