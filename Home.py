0187b497006c585cd62ee71cb92d855202e169fc
chingu647 <chingu647@gmail.com>
2026-07-12 12:48:31 +0900

    all

--- Home.py
+++ Home.py
@@ -21,23 +21,23 @@ if "FIXED_PDF_FILENAME" not in st.session_state:
 
 
 # ###########################################################
-from src.ui import render_admin_sidebar
+from src import ui
 
 # 1. 공통 UI 컴포넌트 로드 (사이드바 메뉴 상시 노출)
-render_admin_sidebar()
+ui.render_admin_sidebar()
 
 # 2. 메인 내비게이션 구성
 TABS = ["휴게소 챗봇", "휴게소 성과", "입점업체 지원"]
 current = st.segmented_control("ex 전북본부", TABS, default="휴게소 챗봇", key="tab")
 
 if current == "휴게소 챗봇":
-    from views import service_chatbot
-    service_chatbot.show_chatbot() 
+    from views import service_chatbot as sc
+    sc.show_chatbot() 
 
 elif current == "휴게소 성과":
-    from views import service_overview
-    service_overview.show_overview() 
+    from views import service_overview as so
+    so.show_overview() 
 
 elif current == "입점업체 지원":
-    from views import seller_support
-    seller_support.show_support() 
+    from views import seller_support as ss
+    ss.show_support() 
